999eb938fe1f7781e3b8a133842bdcad1a9a95eb
Maher <qarnimaher@gmail.com>
2026-05-18 06:44:53 +0300

    Fix

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base : process.env.VITE_BASE_PATH || "/Personal-Knowledge-Management-Frontend" ,
   resolve: {
     alias: {
       "@": "/src",
